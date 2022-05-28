5f693016a9cd6f6d7bd13bd1741199ba03407abc
Siris <vincentcmv@gmail.com>
2022-05-28 11:24:56 +0530

    feat: base

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
 import type { NextPage } from 'next';
+import Builder from '../components/layout';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
 	return (
-		<div>
-			<h1 className='text-center text-blue-600'>Embed builder</h1>
-		</div>
+		<>
+			<Head>
+				<title>Embed builder</title>
+				<link rel='preconnect' href='https://fonts.googleapis.com' />
+				<link rel='preconnect' href='https://fonts.gstatic.com' />
+				<link
+					href='https://fonts.googleapis.com/css2?family=Whitney:wght@100;300;400;500;600;800&display=swap'
+					rel='stylesheet'
+				/>
+			</Head>
+			<div className='flex flex-col justify-center items-center'>
+				<h1 className='text-center text-red-500 text-4xl p-4 m-4 font-semibold'>Embed builder</h1>
+				<Builder />
+			</div>
+		</>
 	);
 };
 
